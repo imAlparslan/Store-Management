80f3a8d100458bf67abe89832d7e5101ed4b7e76
Alparslan Yazıcı <40745160+imAlparslan@users.noreply.github.com>
2024-09-02 16:37:36 +0300

    adding ProductAggregate

---
+++ CatalogManagement/src/CatalogManagement.Domain/ProductAggregate/ValueObjects/ProductDefinition.cs
@@ -0,0 +1,18 @@
+﻿using Ardalis.GuardClauses;
+using CatalogManagement.Domain.Common.Models;
+
+namespace CatalogManagement.Domain.ProductAggregate.ValueObjects;
+public sealed class ProductDefinition : ValueObject
+{
+    public string Value { get; private set; }
+    public ProductDefinition(string definition)
+    {
+        Value = Guard.Against.NullOrWhiteSpace(definition);
+    }
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+
+    public static implicit operator string(ProductDefinition definition) => definition.Value;
+}
---
+++ CatalogManagement/src/CatalogManagement.Domain/ProductAggregate/ValueObjects/ProductId.cs
@@ -0,0 +1,22 @@
+﻿using CatalogManagement.Domain.Common.Models;
+
+namespace CatalogManagement.Domain.ProductAggregate.ValueObjects;
+public sealed class ProductId : ValueObject
+{
+    public Guid Value { get; }
+
+    private ProductId(Guid Id)
+    {
+        Value = Id;
+    }
+    public static ProductId CreateUnique()
+    {
+        return new ProductId(Guid.NewGuid());
+    }
+    public override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Value;
+    }
+    public static implicit operator Guid(ProductId Id) => Id.Value;
+
+}
