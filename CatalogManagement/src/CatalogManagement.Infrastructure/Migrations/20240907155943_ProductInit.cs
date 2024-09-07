7e8b5b68e33b793e434bca0f46a7b08933c915b3
Alparslan Yazıcı <40745160+imAlparslan@users.noreply.github.com>
2024-09-07 19:05:53 +0300

    adding Product migration

---
+++ CatalogManagement/src/CatalogManagement.Infrastructure/Migrations/20240907155943_ProductInit.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CatalogManagement.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ProductInit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "Catalog");
+
+            migrationBuilder.CreateTable(
+                name: "Product",
+                schema: "Catalog",
+                columns: table => new
+                {
+                    ProductId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Definition = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product",
+                schema: "Catalog");
+        }
+    }
+}
