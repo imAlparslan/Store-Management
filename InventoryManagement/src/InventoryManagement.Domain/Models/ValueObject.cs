5a6b116130b4a8cdc391a0371779a16274869149
Alparslan Yazıcı <40745160+imAlparslan@users.noreply.github.com>
2025-04-12 17:54:14 +0300

    inventory context startnig (#11)

---
+++ InventoryManagement/src/InventoryManagement.Domain/Models/ValueObject.cs
@@ -0,0 +1,37 @@
+﻿namespace InventoryManagement.Domain.Models;
+public abstract class ValueObject : IEquatable<ValueObject>
+{
+    public abstract IEnumerable<object> GetEqualityComponents();
+
+    public override int GetHashCode()
+    {
+        return GetEqualityComponents()
+            .Select(x => x != null ? x.GetHashCode() : 0)
+            .Aggregate((x,y) => x ^ y);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null || GetType() != obj.GetType())
+        {
+            return false;
+        }
+
+        return ((ValueObject)obj).GetEqualityComponents()
+            .SequenceEqual(GetEqualityComponents());
+    }
+    public bool Equals(ValueObject? other)
+    {
+        return Equals((object?)other);
+    }
+
+    public static bool operator ==(ValueObject? left, ValueObject? right)
+    {
+        return Equals(left, right);
+    }
+    public static bool operator !=(ValueObject? left, ValueObject? right)
+    {
+        return !Equals(left, right);
+    }
+
+}
---
+++ InventoryManagement/src/InventoryManagement.Infrastructure/Extensions/DependenctInjection.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagement.Infrastructure.Extensions
+{
+    public static class DependenctInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
