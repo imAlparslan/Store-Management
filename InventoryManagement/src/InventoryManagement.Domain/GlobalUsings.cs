2e790579bdefdd6faa3be5dbfc1fc21d86e689d7
Alparslan Yazıcı <40745160+imAlparslan@users.noreply.github.com>
2025-04-19 18:53:56 +0300

    inventory context fixed

--- InventoryManagement/src/InventoryManagement.Domain/GlobalUsings.cs
+++ InventoryManagement/src/InventoryManagement.Domain/GlobalUsings.cs
@@ -1,4 +1,1 @@
-﻿global using SharedKernel.Common.Erros;
-global using SharedKernel.Domain.Common.DomainEventAbstraction;
-global using SharedKernel.Domain.Common.Exceptions;
-global using SharedKernel.Domain.Common.Models;
+﻿global using SharedKernel.Domain.Common.Models;
